8de38a9baaae622258b5497c3faeb2a93f81d8e4
leo-mackbook-pro <leoskywork@qq.com>
2019-04-22 22:10:53 +0800

    make home page responsive by flexbox and media query

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,59 @@
 
-function init(){
-	
-	loadLiveTime();
-	loadingTimer(30 * 60);
-}
+// eslint-disable-next-line no-unused-vars
+function main () {
+	init();
+	test();
 
-
-function loadingTimer(durationSeconds){
-	setTimeout(function(){
-		loadLiveTime();
-		
-		
-		loadingTimer(durationSeconds);
-	}, durationSeconds * 1000);
-}
-
-
-function loadLiveTime()
-{
-	var xmlhttp;
-	if (window.XMLHttpRequest)
-	{// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
+	function test () {
+	// const abc = Date.now()
+	// console.log('object :', object);
+		// let a = 'a test string';
 	}
-	else
-	{// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+
+	function init () {
+		// get live time locally first
+		updateUILiveTime(getLiveTimeLocally());
+		const onSuccess = (responseText) => updateUILiveTime(parseInt(responseText));
+		getLiveTime(onSuccess);
+		getLiveTimeTimer(30 * 60, onSuccess);
 	}
-	  
-	xmlhttp.onreadystatechange=function()
-	{
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-			document.getElementById("website-live-time").innerHTML= " " + xmlhttp.responseText + " days";
+
+	function getLiveTimeLocally () {
+		const now = Date.now();
+		const liveDate = new Date('2018-12-10');
+		const days = Math.ceil((now - liveDate) / 1000 / 60 / 60 / 24);
+		return days;
+	}
+
+	function getLiveTimeTimer (durationSeconds, onSuccess) {
+		setTimeout(function () {
+			getLiveTime(onSuccess);
+			getLiveTimeTimer(durationSeconds, onSuccess);
+		}, durationSeconds * 1000);
+	}
+
+	function getLiveTime (onSuccess) {
+		let xmlhttp;
+		if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp = new XMLHttpRequest();
+		} else { // code for IE6, IE5
+			// eslint-disable-next-line no-undef
+			xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+		}
+
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+				onSuccess(xmlhttp.responseText);
+			}
+		};
+
+		xmlhttp.open('GET', '/api/app/age', true);
+		xmlhttp.send();
+	}
+
+	function updateUILiveTime (liveTime) {
+		if (Number.isInteger(liveTime)) {
+			document.getElementById('website-live-time').innerHTML = liveTime + ' days';
 		}
 	}
-	xmlhttp.open("GET","/api/app/age",true);
-	xmlhttp.send();
 }
-
